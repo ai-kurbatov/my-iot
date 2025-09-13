5f08fac5a9e6d64799add3b128aa56bc326dece0
Anton Kurbatov <a.kurbatov@protonmail.com>
2025-09-13 15:30:54 +0300

    add force update example

--- module_template/module_template.ino
+++ module_template/module_template.ino
@@ -5,17 +5,20 @@
 #include "module_template.hpp"
 
 size_t SWITCH_COUNT_ST = -1;
+size_t D5_ST = -1;
 
 size_t BLINKING_INTERVAL_SE = -1;
 
 void setup_module() {
+  pinMode(D5, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
+
   SERVER.on("/hello", HTTP_ANY, [](){SERVER.send(200, "text/plain", "Hello world!");});
 
   SWITCH_COUNT_ST = STATE.add("Switch count", 0);
+  D5_ST = STATE.add("D5 pin state", static_cast<bool>(digitalRead(D5)));
 
   BLINKING_INTERVAL_SE = SETTINGS.add("Blinking interval", 500);
-
-  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop_module() {
@@ -27,4 +30,7 @@ void loop_module() {
   STATE[SWITCH_COUNT_ST] = STATE[SWITCH_COUNT_ST].asInt() + 1;
 }
 
-void force_update_state_module() {}
+void force_update_state_module() {
+  // Update the value to see the current state in /state
+  STATE[D5_ST] = static_cast<bool>(digitalRead(D5));
+}
